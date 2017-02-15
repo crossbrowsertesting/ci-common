4b10395577f5d8d6ce7b10e4ffaec23f8d05808b
Michael Hollister <hollister.michael@gmail.com>
2017-02-15 16:52:01 -0600

    changed logic to get a selenium test id

--- src/main/java/com/crossbrowsertesting/api/Selenium.java
+++ src/main/java/com/crossbrowsertesting/api/Selenium.java
@@ -1,7 +1,7 @@
 package com.crossbrowsertesting.api;
 
 import java.io.IOException;
-
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.LinkedList;
@@ -130,6 +130,66 @@ public class Selenium extends ApiFactory{
 		String json = req.get("", params);
 		return parseIdAndPublicUrl(json);
 	}
+	public String getSeleniumTestId(String name, String build, String browserApiName, String osApiName, String resolution) throws IOException {
+		/*
+		 * ------------ WARNING ---------------------
+		 * this may return the wrong selenium test id. it's really only intended for analytics
+		 * use getSeleniumTestInfo or getSeleniumTestInfo2 for an actual selenium_test_id if you have valid parameters
+		 */
+		String seleniumTestId = "";
+		Map<String, String> params = new HashMap<String, String>();
+		boolean done = false;
+		for (int tryCount = 1; tryCount <= 3 && !done; tryCount++) {
+			seleniumTestId = "";
+			params.put("name", name);
+			params.put("build", build);
+			OperatingSystem os = operatingSystems2.get(osApiName);
+			try {
+				params.put("os", os.getName());
+				params.put("browser", os.browsers2.get(browserApiName).getName());
+				params.put("resolution",resolution);
+			} catch (NullPointerException npe) { // if we get a NullPointer some where lets just assume its the 3rd case
+				params.put("os", "");
+				params.put("resolution", "");
+				params.put("browser", "");
+			}
+			switch (tryCount) {
+				case 1:
+					// use all the configuration params on the first try
+					break;
+				case 2:
+					// use without the name and build number
+					params.remove("name");
+					params.remove("build");
+					break;
+				case 3:
+					// last try... just use any test that doesnt already have jenkins in the client_platform
+					params.clear();
+					break;
+			}
+			String json = req.get("", params);
+			//got the test now need to parse out the id and publicUrl
+			JSONObject j = new JSONObject(json);
+			JSONArray seleniumTests = j.getJSONArray("selenium");
+			for(int i=0; i < seleniumTests.length() && !done;i++) {
+				JSONObject seleniumTest = seleniumTests.getJSONObject(i);
+				String clientPlatform = seleniumTest.getString("client_platform");
+				String tmp_seleniumTestId = Integer.toString(seleniumTest.getInt("selenium_test_id"));
+				if (tryCount == 1 && !tmp_seleniumTestId.isEmpty()) {
+					done = true;
+					seleniumTestId = tmp_seleniumTestId;
+				} else if (tryCount > 1 && (!tmp_seleniumTestId.isEmpty() && !clientPlatform.contains("jenkins"))) {
+					done = true;
+					seleniumTestId = tmp_seleniumTestId;
+				}
+			}
+		}
+		return seleniumTestId;
+	}
+	public Queue<Map<String, String>> getSeleniumTestInfo2(Map<String, String> params) throws IOException {
+		String json = req.get("", params);
+		return parseIdAndPublicUrl(json);
+	}
 	public Queue<Map<String, String>> getSeleniumTestInfo2(String name, String build, String browserApiName, String osApiName, String resolution) throws IOException {
 		Map<String, String> params = new HashMap<String, String>();
 		params.put("name", name);
@@ -138,8 +198,7 @@ public class Selenium extends ApiFactory{
 		params.put("os", os.getName());
 		params.put("browser", os.browsers2.get(browserApiName).getName());
 		params.put("resolution",resolution);
-		String json = req.get("", params);
-		return parseIdAndPublicUrl(json);
+		return getSeleniumTestInfo2(params);
 	}
 	private Queue<Map<String, String>> parseIdAndPublicUrl(String json) {
 		//got the test now need to parse out the id and publicUrl
@@ -157,6 +216,7 @@ public class Selenium extends ApiFactory{
 		}
 		return tests;
 	}
+	
 	private String apiSetAction(String seleniumTestId, String action, String param, String value) throws IOException {
 		/*
 		 * param and value are the additional parameters for actions
@@ -179,6 +239,7 @@ public class Selenium extends ApiFactory{
 			apiSetAction(seleniumTestId, "set_score", "score", "fail");
 		}	
 	}
+	
 	public void updateContributer(String seleniumTestId, String contributer, String contributerVersion, String pluginVersion) throws IOException {
 		/*
 		 * contributer looks like "jenkins1.5|v0.21"
