ae772816536352b91d3e5a5ea56f5e4c9b54d5bd
Michael Hollister <hollister.michael@gmail.com>
2017-08-07 18:57:13 -0500

    fixing

--- src/main/java/com/crossbrowsertesting/api/LocalTunnel.java
+++ src/main/java/com/crossbrowsertesting/api/LocalTunnel.java
@@ -1,13 +1,10 @@
 package com.crossbrowsertesting.api;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -202,7 +199,7 @@ public class LocalTunnel extends ApiFactory {
 		/*
 		 * Runs a subprocess that starts the node local tunnel using a custom path
 		 */
-		start(localTunnelPath, new HashMap<String, String>());
+		start("\""+localTunnelPath+"\"", new HashMap<String, String>());
 	}
 	@Deprecated
 	public void start() throws IOException {
